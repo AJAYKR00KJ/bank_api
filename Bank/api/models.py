a96dd2c0b87d6953a80cf09f50e5af33a2f17587
Ajay <ajaykr00kj@gmail.com>
2021-05-17 23:06:48 +0530

    initialize by creating models

---
+++ Bank/api/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+# Create your models here.
+class BranchInfo(models.Model):
+
+    ifsc = models.CharField(max_length=100)
+    bank_id = models.IntegerField()
+    branch = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    district = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    bank_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.bank_id
